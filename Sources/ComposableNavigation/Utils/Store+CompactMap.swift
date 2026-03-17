34bee296f562ce485bc82b436265765dc8e37542
Cristian Ortega <corteggo@outlook.es>
2026-03-17 10:46:44 +0100

    Revert a change that is causing crashes
    To be squashed

--- Sources/ComposableNavigation/Utils/Store+CompactMap.swift
+++ Sources/ComposableNavigation/Utils/Store+CompactMap.swift
@@ -4,15 +4,18 @@ public extension Store where State: Equatable {
 	func compactMap<NonOptionalState, Unwrapped>(
 		_ transform: (Store<NonOptionalState, Action>) -> Unwrapped
 	) -> Unwrapped? where State == NonOptionalState? {
-        guard let _ = withState({ $0 }) else {
-            return nil
-        }
-        
-        return transform(
-            scope(
-                state: \.unsafelyUnwrapped,
-                action: \.self
-            )
-        )
+        if var state = self.withState({ $0 }) {
+			return transform(
+				self.scope(
+					state: {
+						state = $0 ?? state
+						return state
+					},
+					action: { $0 }
+				)
+			)
+		} else {
+			return nil
+		}
 	}
 }
