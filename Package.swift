352d424c19988b8f26d271d80e11376c721eae31
Christoph Edthofer <christoph.edthofer@roche.com>
2026-06-17 11:17:13 +0200

    Set minimal iOS version to 16

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
 	name: "ComposableNavigation",
-	platforms: [.iOS(.v13)],
+	platforms: [.iOS(.v16)],
 	products: [
 		.library(
 			name: "ComposableNavigation",
